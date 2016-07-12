9d1ab4e376f0a258ff937618b676203d266315a0
Dmitry Bezzubenkov <dbezzubenkov@dev-team.com>
2016-07-13 00:52:31 +0300

    requiresslattribute source code

---
+++ RequireSSLAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace DevTeam.RequireSSLAttribute
+{
+    public class RequireSSLAttribute : AuthorizationFilterAttribute
+    {
+        public override void OnAuthorization(HttpActionContext actionContext)
+        {
+            var request = actionContext.Request;
+            if (request.RequestUri.Scheme != Uri.UriSchemeHttps)
+            {
+                actionContext.Response = new HttpResponseMessage(HttpStatusCode.Forbidden);
+            }
+        }
+    }
+}
